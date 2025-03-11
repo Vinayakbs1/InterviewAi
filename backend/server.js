ce432b578147c6c7f3a822f50a510248caf4800e
Vinayakbs1 <154055270+Vinayakbs1@users.noreply.github.com>
2025-03-11 19:33:50 +0000

    server

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // Configure CORS for localhost only
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://interviewai-rvh6.onrender.com'
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
